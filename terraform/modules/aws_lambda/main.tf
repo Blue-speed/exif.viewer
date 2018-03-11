532615d3398aac28496f7f83bbbfac3a94f88921
Bluespeedgamer <github@bluespeed.us>
2018-03-11 10:37:38 -0400

    remove empty environments section for lambda

--- terraform/modules/aws_lambda/main.tf
+++ terraform/modules/aws_lambda/main.tf
@@ -7,10 +7,6 @@ resource "aws_lambda_function" "Lambda" {
   runtime          = "dotnetcore2.0"
   timeout          = "20"
   publish          = "true"
-  environment {
-    variables = {
-    }
-  }
   vpc_config = {
     subnet_ids = ["subnet-dec1c4f4", "subnet-28e9ec70", "subnet-cb5fedc7", "subnet-1134ba74"]
     security_group_ids = ["sg-d152bdab"]
