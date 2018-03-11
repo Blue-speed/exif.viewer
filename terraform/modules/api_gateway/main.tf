56ab358c119641111dde3e6a3db9496cd51a84e4
Bluespeedgamer <github@bluespeed.us>
2018-03-11 18:05:45 -0400

    forgot the response

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -27,6 +27,13 @@ resource "aws_api_gateway_integration" "ApiRootIntegration" {
   content_handling        = "CONVERT_TO_TEXT"
 }
 
+resource "aws_api_gateway_integration_response" "ApiIntegrationProxyResponse" {
+    rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
+    resource_id = "${aws_api_gateway_resource.root.id}"
+    http_method = "${aws_api_gateway_integration.ApiRootIntegration.http_method}"
+    status_code = "200"
+}
+
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.ApiGateway.root_resource_id}"
