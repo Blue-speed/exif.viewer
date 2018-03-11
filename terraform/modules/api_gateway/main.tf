43c5d36ee307fadafcb897c256910959203429ee
Bluespeedgamer <github@bluespeed.us>
2018-03-11 18:04:50 -0400

    dedicated root part

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -2,6 +2,31 @@ resource "aws_api_gateway_rest_api" "ApiGateway" {
   name        = "${var.gatewayName}"
 }
 
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.ApiGateway.root_resource_id}"
+  path_part   = "/"
+}
+
+resource "aws_api_gateway_method" "ApiRootMethod" {
+  rest_api_id         = "${aws_api_gateway_rest_api.ApiGateway.id}"
+  resource_id         = "${aws_api_gateway_resource.root.id}"
+  http_method         = "ANY"
+  authorization       = "NONE"
+  request_parameters  = {"method.request.path.proxy" = true}
+}
+
+resource "aws_api_gateway_integration" "ApiRootIntegration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.ApiGateway.id}"
+  resource_id             = "${aws_api_gateway_resource.root.id}"
+  http_method             = "${aws_api_gateway_method.ApiRootMethod.http_method}"
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambdaArn}/invocations"
+  content_handling        = "CONVERT_TO_TEXT"
+}
+
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.ApiGateway.root_resource_id}"
@@ -35,7 +60,7 @@ resource "aws_api_gateway_integration_response" "ApiIntegrationProxyResponse" {
 }
 
 resource "aws_api_gateway_deployment" "ApiDeployment" {
-  depends_on = ["aws_api_gateway_method.ApiProxyMethod", "aws_api_gateway_integration.ApiProxyIntegration" ]
+  depends_on = ["aws_api_gateway_method.ApiProxyMethod","aws_api_gateway_method.ApiRootMethod", "aws_api_gateway_integration.ApiProxyIntegration", "aws_api_gateway_integration.ApiRootIntegration" ]
   rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
   stage_name = "${var.stageName}"
   description = "Api gateway for  ${var.gatewayName}"
