1b5cfa952c470bc629083e8b1401cf8a9b364e6c
Bluespeedgamer <github@bluespeed.us>
2018-03-11 19:27:09 -0400

    endless derpage

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -2,6 +2,12 @@ resource "aws_api_gateway_rest_api" "ApiGateway" {
   name        = "${var.gatewayName}"
 }
 
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.ApiGateway.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
 resource "aws_api_gateway_method" "ApiProxyMethod" {
   rest_api_id         = "${aws_api_gateway_rest_api.ApiGateway.id}"
   resource_id         = "${aws_api_gateway_resource.proxy.id}"
@@ -29,7 +35,7 @@ resource "aws_api_gateway_integration_response" "ApiIntegrationProxyResponse" {
 }
 
 resource "aws_api_gateway_deployment" "ApiDeployment" {
-  depends_on = ["aws_api_gateway_method.ApiProxyMethod", "aws_api_gateway_integration.ApiProxyIntegration" ]
+  depends_on = ["aws_api_gateway_method.ApiProxyMethod","aws_api_gateway_integration.ApiProxyIntegration" ]
   rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
   stage_name = "${var.stageName}"
   description = "Api gateway for  ${var.gatewayName}"
