b46fe8655fa3da4dd80f7c51ee007607ed3f778f
Bluespeedgamer <github@bluespeed.us>
2018-03-11 19:17:46 -0400

    fix the thing

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -11,7 +11,6 @@ resource "aws_api_gateway_method" "ApiRootMethod" {
 
 resource "aws_api_gateway_integration" "ApiRootIntegration" {
   rest_api_id             = "${aws_api_gateway_rest_api.ApiGateway.id}"
-  resource_id             = "${aws_api_gateway_resource.root.id}"
   http_method             = "${aws_api_gateway_method.ApiRootMethod.http_method}"
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
@@ -22,7 +21,6 @@ resource "aws_api_gateway_integration" "ApiRootIntegration" {
 
 resource "aws_api_gateway_integration_response" "ApiIntegrationRootResponse" {
     rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
-    resource_id = "${aws_api_gateway_resource.root.id}"
     http_method = "${aws_api_gateway_integration.ApiRootIntegration.http_method}"
     status_code = "200"
 }
