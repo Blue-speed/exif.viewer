1d910cff366f0b35b937a301dfd892fc2080d4fd
Bluespeedgamer <github@bluespeed.us>
2018-03-11 19:11:25 -0400

    fix path part

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api" "ApiGateway" {
 resource "aws_api_gateway_resource" "root" {
   rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.ApiGateway.root_resource_id}"
-  path_part   = "/"
+  path_part   = ""
 }
 
 resource "aws_api_gateway_method" "ApiRootMethod" {
