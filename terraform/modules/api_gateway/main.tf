386fba210081498c927bbe7b139aba3faeb4fe4d
Bluespeedgamer <github@bluespeed.us>
2018-03-11 19:08:20 -0400

    Fix duplicate name error

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -27,7 +27,7 @@ resource "aws_api_gateway_integration" "ApiRootIntegration" {
   content_handling        = "CONVERT_TO_TEXT"
 }
 
-resource "aws_api_gateway_integration_response" "ApiIntegrationProxyResponse" {
+resource "aws_api_gateway_integration_response" "ApiIntegrationRootResponse" {
     rest_api_id = "${aws_api_gateway_rest_api.ApiGateway.id}"
     resource_id = "${aws_api_gateway_resource.root.id}"
     http_method = "${aws_api_gateway_integration.ApiRootIntegration.http_method}"
