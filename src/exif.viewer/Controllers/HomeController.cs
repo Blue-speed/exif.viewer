18173a0ab6c35b0539dc490c6dd4901fea0a0140
Bluespeedgamer <github@bluespeed.us>
2018-03-12 20:03:53 -0400

    Add presentation pdf to source

--- src/exif.viewer/Controllers/HomeController.cs
+++ src/exif.viewer/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
-using SixLabors.ImageSharp;
 using Microsoft.AspNetCore.Http;
 using exif.viewer.Models;
 using System.Diagnostics;
